f41c1d1344a137d175542a6d4f97d7631d0c361d
Frank Seifferth <frankseifferth@posteo.net>
2022-03-13 13:37:36 +0100

    Add sampling capabilities to ttm comp

--- ttm/comp.py
+++ ttm/comp.py
@@ -5,20 +5,19 @@ from .types import *
 from .eval import cluster_distribution, bucket_probability
 from itertools import combinations
 
-def _cluster2doc(f: InputFile) -> dict:
+def _cluster2doc(f: InputFile, sample: set) -> dict:
     cluster2doc = { c: [] for c in set(f.column('cluster')) }
     for doc, cluster in zip(f.column('id'), f.column('cluster')):
-        cluster2doc[cluster].append(doc)
+        if doc in sample: cluster2doc[cluster].append(doc)
     return cluster2doc
-def _cluster_pairs(f: InputFile):
-    for _cluster, docs in _cluster2doc(f).items():
+def _cluster_pairs(f: InputFile, sample: set):
+    for _cluster, docs in _cluster2doc(f, sample).items():
         for c in combinations(sorted(docs), 2):
             yield c
-def _kappa(f: InputFile, g: InputFile) -> tuple:
-    ids = list(f.column('id'))
-    U = list(combinations(ids, 2))
-    F = set(_cluster_pairs(f))
-    G = set(_cluster_pairs(g))
+def _kappa(f: InputFile, g: InputFile, sample: set) -> tuple:
+    U = list(combinations(sample, 2))
+    F = set(_cluster_pairs(f, sample=sample))
+    G = set(_cluster_pairs(g, sample=sample))
     #
     p_o = 1 - ( len(F.symmetric_difference(G)) / len(U) )
     p_F = bucket_probability(cluster_distribution(f.column('cluster')))
@@ -28,15 +27,15 @@ def _kappa(f: InputFile, g: InputFile) -> tuple:
     kappa = (p_o - p_e) / (1 - p_e)
     zoom = 1 / (1 - p_e)
     return (kappa, zoom)
-def _kappa(f: InputFile, g: InputFile) -> tuple:
+def _kappa(f: InputFile, g: InputFile, sample: set) -> tuple:
     """
     Memory-optimized version for large kappas (also seems to run faster)
     """
-    n_ids = sum((1 for _ in f.column('id')))
+    n_ids = len(sample)
     len_U = (n_ids**2 - n_ids) / 2    # Matrix of id x id minus diagonal and
                                       # with the mirrored halfs deduplicated
-    F = _cluster_pairs(f)
-    G = _cluster_pairs(g)
+    F = _cluster_pairs(f, sample=sample)
+    G = _cluster_pairs(g, sample=sample)
     diff_F_G = set(F)
     for pair in G:               # Memory-efficient symmetric set difference
         if pair in diff_F_G:
@@ -52,12 +51,23 @@ def _kappa(f: InputFile, g: InputFile) -> tuple:
     kappa = (p_o - p_e) / (1 - p_e)
     zoom = 1 / (1 - p_e)
     return (kappa, zoom)
-def avg_kappa(*infiles: InputFile) -> tuple:
+def avg_kappa(*infiles: InputFile, sample_size=1., repeat=1) -> tuple:
+    import random
+    docs = None
+    for f in infiles:
+        if docs == None:
+            docs, last_filename = sorted(f.column('id')), f.filename
+        else:
+            if sorted(f.column('id')) != docs: raise ExpectedRuntimeError(
+                'Found differences in document ids between '
+               f"'{last_filename}' and '{f.filename}'")
     kappas, zooms = [], []
-    for f, g in combinations(infiles, 2):
-        k, z = _kappa(f, g)
-        kappas.append(k)
-        zooms.append(z)
+    for i in range(repeat):
+        sample = set(random.sample(docs, round(sample_size*len(docs))))
+        for f, g in combinations(infiles, 2):
+            k, z = _kappa(f, g, sample=sample)
+            kappas.append(k)
+            zooms.append(z)
     avg_k = sum(kappas)/len(kappas)
     dev_k = (sum( (k - avg_k)**2 for k in kappas) / len(kappas))**.5
     avg_z = sum(zooms)/len(zooms)
@@ -86,22 +96,49 @@ Evaluation Metrics
                 suggestion from 1960.
 
 Command Options
-    -h, --help  Print this help message and exit.
+    --sample-size N
+                The relative sample size to draw from the data when
+                calculating avg-kappa. This value must lie between 0
+                and 1. Default: 1.
+    --repeat N
+                Calculate kappa N times for each pair of files. This
+                can be used to improve the accuracy of results for
+                small sample sizes. Default: 1.
+    -h, --help
+                Print this help message and exit.
 """.lstrip()
 
 def _cli(argv, infile, outfile):
-    opts, filenames = getopt(argv, 'h', ['help'])
+    opts, filenames = getopt(argv, 'h', ['help', 'sample-size=', 'repeat='])
     short2long = { '-h': '--help' }
-    opts = { short2long.get(k, k).lstrip('-'): v for k, v in opts }
+    opts = { short2long.get(k, k).lstrip('-').replace('-', '_'): v
+             for k, v in opts }
     if 'help' in opts:
         raise HelpRequested(_cli_help)
     if len(filenames) < 2:
         raise CliError('At least two FILE arguments are required '
                        'for ttm comp')
+    for k in ['sample_size']:
+        if k not in opts:
+            opts[k] = 1.
+        else:
+            opts[k] = float(opts[k])
+            if opts[k] < 0 or opts[k] > 1:
+                raise CliError('--sample-size must lie between 0 and 1')
+    for k in ['repeat']:
+        if k not in opts: opts[k] = 1
+        else: opts[k] = int(opts[k])
     files = [ InputFile(f) for f in filenames ]
     print('models              ', end='', flush=True)
     print(f'{len(files)}    {filenames}')
+    print(f'sample-size         {opts["sample_size"]}  ', end='', flush=True)
+    n_samples = round(opts['sample_size']*len(files[0].column('id')))
+    print(f'({n_samples} samples)')
+    print(f'comparisons         {len(files)*opts["repeat"]}  '
+          f'({len(list(combinations(files, 2)))} model combinations, '
+          f'{opts["repeat"]} repetition{"s" if opts["repeat"] > 1 else ""}'
+           ' each)')
     print('avg-kappa           ', end='', flush=True)
-    avg_k, dev_k, avg_z, dev_z = avg_kappa(*files)
+    avg_k, dev_k, avg_z, dev_z = avg_kappa(*files, **opts)
     print(f'{avg_k:.4f} \u00B1{dev_k:.4f}  '
           f'(zoom {avg_z:.2f} \u00B1{dev_z:.2f})')
