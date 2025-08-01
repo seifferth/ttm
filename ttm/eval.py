ab2ac99d4f6244ea113c143e87fc3685c0c83927
Frank Seifferth <frankseifferth@posteo.net>
2025-08-01 14:35:53 +0200

    Allow calculating psq-distance without psq-score

--- ttm/eval.py
+++ ttm/eval.py
@@ -340,7 +340,6 @@ def _print_tsv(r: EvaluationResult):
     if r.psq_distance == None:
         r.psq_distance, r.psq_distance_sample_size = 'undefined', 'undefined'
     if r.psq_count == None:
-        r.psq_distance, r.psq_distance_sample_size = 'N/A', 'N/A'
         r.psq_count = 'N/A'
         r.psq_score, r.psq_score_zoom = 'N/A', 'N/A'
     elif r.psq_score == None:
